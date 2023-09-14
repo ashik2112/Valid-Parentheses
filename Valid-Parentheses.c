14978f672cbeff9f082f951dd6bc108bb16a3253
Md. Asaduzzaman Ashik <135534589+ashik2112@users.noreply.github.com>
2023-09-14 15:07:29 +0600

    Create Valid-Parentheses.c

---
+++ Valid-Parentheses.c
@@ -0,0 +1,23 @@
+bool isValid(char *s)
+{
+
+    char *q=s;
+
+    for (char *p=s; *p; p++)
+        switch(*p)
+        {
+        case '(':
+            *q++ = ')';
+            continue;
+        case '{':
+            *q++ = '}';
+            continue;
+        case '[':
+            *q++ = ']';
+            continue;
+        default:
+            if (q==s || *p != *--q) return false;
+        }
+
+    return q==s;
+}
